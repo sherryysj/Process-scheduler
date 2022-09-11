7de8d70fc75bf4503d131a693aba2036cc9fc6e6
sherryysj <45263452+sherryysj@users.noreply.github.com>
2022-09-12 00:17:02 +1000

    implement ordered array rq

--- OrderedArrayRQ.py
+++ OrderedArrayRQ.py
@@ -1,19 +1,48 @@
+import Process
+
+
 class OrderedArrayRQ:
     def __init__(self):
+        self.queue = []
 
-    def enqueue(procLabel, vt):
+    def enqueue(self, procLabel, vt):
+        for i in len(self.queue):
+            if (vt < self.queue[i].vt):
+                self.queue.insert(i, Process(procLabel, vt))
+
+    def dequeue(self):
+        if len(self.queue) > 0:
+            procLabel = self.queue[0].procLabel
+            self.queue.pop[0]
+            return procLabel
         return ''
 
-    def dequeue():
-        return ''
-
-    def findProcess(procLabel):
+    def findProcess(self, procLabel):
+        for process in self.queue:
+            if (process.proLabel == procLabel):
+                return True
         return False
 
-    def removeProcess(procLabel):
+    def removeProcess(self, procLabel):
+        for process in self.queue:
+            if (process.proLabel == procLabel):
+                self.queue.remove(process)
+                return True
+        return False
+
+    def succedingProcessTime(self, procLabel):
+        for i in len(self.queue):
+            if (self.queue[i].proLabel == procLabel):
+                processTime = 0
+                startIndex = i + 1
+                processAmount = len(self.queue) - startIndex
+                for j in processAmount:
+                    processTime += self.queue[j+startIndex]
+                return processTime
         return -1
 
-    def succedingProcessTime(procLabel):
-        return -1
-
-    def printAllProcesses():
+    def printAllProcesses(self):
+        if len(self.queue) > 0:
+            for process in self.queue:
+                print(process.procLabel)
+        return ''
