86a9438eead1e7732835d360ffbbf615561e64fb
sherryysj <45263452+sherryysj@users.noreply.github.com>
2022-09-11 22:03:00 +1000

    set template for ordered array runqueue

---
+++ OrderedArrayRQ.py
@@ -0,0 +1,19 @@
+class OrderedArrayRQ:
+    def __init__(self):
+
+    def enqueue(procLabel, vt):
+        return ''
+
+    def dequeue():
+        return ''
+
+    def findProcess(procLabel):
+        return False
+
+    def removeProcess(procLabel):
+        return -1
+
+    def succedingProcessTime(procLabel):
+        return -1
+
+    def printAllProcesses():
